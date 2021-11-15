e6282c2aa63666f0f67e457a582b7fba05886213
KPMGE <jesuskevin254@gmail.com>
2021-11-15 18:21:34 -0300

    Starting project

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
