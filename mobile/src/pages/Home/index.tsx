57fbd62dd0b08433471cf3ce19fb819d4f385d7c
KPMGE <jesuskevin254@gmail.com>
2021-11-21 10:03:11 -0300

    updating fetch location

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
 import { Backgroud } from "../../components/Backgroud";
 import { ListTasks } from "../../components/ListTasks";
 import { AddTasksButtons } from "../../components/AddTasksButtons";
 import { CircleButton } from "../../components/CircleButton";
 import { TodayTasks } from "../../components/TodayTasks";
+import { ListType } from "../../@types";
+import api from "../../services/api";
 
 import styles from "./styles";
 
 export const Home = () => {
   const [showAddButtons, setShowAddButtons] = useState(false);
+  const [lists, setLists] = useState<ListType[]>([]);
 
   const toggleShowAddButtons = () => {
     setShowAddButtons(!showAddButtons);
   };
+
+  useEffect(() => {
+    const getAllLists = async () => {
+      const response = await api.get("/lists");
+      setLists(response.data);
+    };
+
+    getAllLists();
+  }, []);
 
   return (
     <Backgroud>
@@ -26,7 +38,7 @@ export const Home = () => {
 
         <View style={styles.listTasksContainer}>
           <Text style={styles.lists}>Lists</Text>
-          <ListTasks />
+          <ListTasks lists={lists} />
         </View>
 
         {!showAddButtons && (
